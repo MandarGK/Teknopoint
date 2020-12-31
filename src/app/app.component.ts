cee1c1d41434dbbbd29020c475e692c683f891eb
MandarGK <mandarkale5@gmail.com>
2020-12-31 17:09:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  navbarOpen =false;
+
+  toggleNavbar(){
+    this.navbarOpen =!this.navbarOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlansComponent } from './plans/plans.component';
+import { ContactComponent } from './contact/contact.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlansComponent,
+    ContactComponent,
+    SolutionsComponent,
+    CustomerComponent,
+    AboutComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
